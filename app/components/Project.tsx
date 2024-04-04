fbab114cf5c97734de11adcd7ef5079974b5de6e
modest-azad <collezian@gmail.com>
2024-04-04 22:50:39 +0530

    project card local API added

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -1,5 +1,31 @@
+'use client'
 import Image from "next/image"
+import { useState } from "react";
+
+const data = [
+    {
+        tittle: "TechKitiz UI",
+        subtittle: "designing",
+        link: "https://the-mdazad.vercel.app/",
+        image: "/Graphic.png",
+    },
+    {
+        tittle: "TechKitiz UI",
+        subtittle: "development",
+        link: "https://techkshitiz.onrender.com/",
+        image: "/Graphic.png",
+    },
+    {
+        tittle: "TechKitiz UI",
+        link: "https://the-mdazad.vercel.app/",
+        image: "/Graphic.png",
+    },
+    
+    
+]
+
 export default function Project() {
+    const [currentData, setData] = useState(data);
 
     return (
         <div className="p-[5%] w-full ">
@@ -9,50 +35,33 @@ export default function Project() {
             </span>
 
             <span className="cards grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-6">
-                <div className="grayscale-0 hover:grayscale transition duration-300 bg-gray-100 rounded-lg overflow-hidden aspect-[1/1] w-full">
-                    <div className="relative h-full bg-gray-200 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
-                        <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300">
-                            <h2 className="text-white text-lg font-semibold">Card Title</h2>
-                        </div>
-                        <div >
-                            {/* <p>Card content goes here.</p> */}
-                            <Image className="w-full" src="/flower1.jpg" alt="image" width={50} height={50} />
-                        </div>
-                    </div>
-                </div>
-                <div className="grayscale-0 hover:grayscale transition duration-300 bg-gray-100 rounded-lg overflow-hidden aspect-[1/1] w-full">
-                    <div className="relative h-full bg-gray-200 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
-                        <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300">
-                            <h2 className="text-white text-lg font-semibold">Card Title</h2>
-                        </div>
-                        <div >
-                            {/* <p>Card content goes here.</p> */}
-                            <Image className="w-full" src="/flower.jpg" alt="image" width={50} height={50} />
-                        </div>
-                    </div>
-                </div>
-                <div className="grayscale-0 hover:grayscale transition duration-300 bg-gray-100 rounded-lg overflow-hidden aspect-[1/1] w-full">
-                    <div className="relative h-full bg-gray-200 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
-                        <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300">
-                            <h2 className="text-white text-lg font-semibold">Card Title</h2>
-                        </div>
-                        <div >
-                            {/* <p>Card content goes here.</p> */}
-                            <Image className="w-full" src="/HeroImage.png" alt="image" width={50} height={50} />
-                        </div>
-                    </div>
-                </div>
-                <div className="grayscale-0 hover:grayscale transition duration-300 bg-gray-100 rounded-lg overflow-hidden aspect-[1/1] w-full">
-                    <div className="relative h-full bg-gray-200 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
-                        <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300">
-                            <h2 className="text-white text-lg font-semibold">Card Title</h2>
-                        </div>
-                        <div >
-                            {/* <p>Card content goes here.</p> */}
-                            <Image className="w-full" src="/ramadan.jpg" alt="image" width={40} height={40} />
-                        </div>
-                    </div>
-                </div>
+                {
+                    data.map((currentData) => {
+                        return (
+                            <div key={1} className="transition duration-300 bg-gray-100 rounded-lg overflow-hidden aspect-[1/1] w-full">
+                                <div className="relative h-full bg-gray-200 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
+                                    <div className=" absolute inset-0 bg-gray-900 bg-opacity-80 flex items-center justify-center flex-col opacity-0 hover:opacity-100 transition duration-300">
+                                        <h2 className="text-white text-lg font-semibold">{currentData.tittle}</h2>
+                                        <span className="flex">
+                                        <a className="text-white text-sm m-2" href="#">
+                                            <Image src="/icons/link.png" alt="icon" width={30} height={30}/>
+                                        </a>                                        
+                                        <a className="text-white  text-sm m-2" href="#">
+                                            <Image src={(currentData.subtittle === "development")? "/icons/github.png" : "/icons/figma.png"} alt="icon" width={30} height={30}/>
+                                        </a>                                        
+                                        </span>
+                                            
+                                    </div>
+                                    <div >
+                                        {/* <p>Card content goes here.</p> */}
+                                        <Image className="w-full" src={currentData.image} alt="image" width={50} height={50} />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
             </span>
         </div>
     )
